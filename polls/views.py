e91ce737a413d39341dbe620d5eb92cbc479b9c1
metr1ckzu <metr1ckzu@gmail.com>
2017-08-02 03:23:52 +0600

    added view for voting, refactored views for index, detail and results

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question
-from django.http import Http404
+from django.http import HttpResponseRedirect
+from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-publication_date')[:5]
 
 
+class DetailView(generic.DetailView):
+    model= Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-publication_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -22,6 +38,8 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+'''
 
 
 def vote(request, question_id):
